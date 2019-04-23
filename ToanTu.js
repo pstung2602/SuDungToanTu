285630b53b59941ba4d6b3b2bd85ea9a1c66b701
pstung2602 <44184240+pstung2602@users.noreply.github.com>
2019-04-23 16:52:21 +0700

    bai tap Toan Tu

---
+++ ToanTu.js
@@ -0,0 +1,6 @@
+let inputWidth = prompt("Nhap chieu rong");
+let inputHeight = prompt("Nhap chieu cao");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("Dien Tich S= " + area);
